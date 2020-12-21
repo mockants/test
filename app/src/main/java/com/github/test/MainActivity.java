4b8307960b95ee95f478cc827770aafc24f4a4d7
linbc <linbc@xiaopeng.com>
2020-12-21 15:50:43 +0800

    init

---
+++ app/src/main/java/com/github/test/MainActivity.java
@@ -0,0 +1,18 @@
+package com.github.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.library.LibManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LibManager.init();
+    }
+}
---
+++ lib/src/main/java/com/github/library/LibManager.java
@@ -0,0 +1,14 @@
+package com.github.library;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * create by linbc
+ * create on 2020-12-19
+ */
+public class LibManager {
+
+    public static void init(){
+        RecyclerView recyclerView ;
+    }
+}
